d3d8e2e968ba5a7da1e4c2ce761f2eeafa4aebef
leo <leo.cluzel@idopte.com>
2026-02-27 19:00:23 +0100

    23 - added new initialization config

--- resources/src/initializer.js
+++ resources/src/initializer.js
@@ -2,20 +2,38 @@ import App_Container from './app/app-container.js';
 import App from './app/app.js';
 import Network_Map_Page from './page/network-map-page.js';
 import Train_Animation from './loader/Train_Animation.js';
+import { App_Config} from "../resources-config/config.js";
+import Timetables_Page from './page/time_tables_page.js';
 
 
 async function Initialize() {
-	let expandingList = App_Container.Create();
-	document.getElementById('root').appendChild(expandingList);
+	let expanding_list = App_Container.Create();
+	document.getElementById('root').appendChild(expanding_list);
 
-	let map_page = Network_Map_Page.Create();
 	let loader = Train_Animation.Create();
-	let app = App.Create(loader,map_page,null);
-	expandingList.Add_App(app);
-	app.Loading();
-	await app.Page.Initialize_Map();
-	app.Loaded();
-	await app.Page.Initial_Zoom_Move();
+	if(App_Config.HAVE_NETWORK_MAP)
+	{
+		let map_page = Network_Map_Page.Create();
+		let app = App.Create(loader,map_page,null);
+		expanding_list.Add_App(app);
+		Initialize_Network(app);
+		app.Loading();
+		await app.Page.Initialize_Map();
+		app.Loaded();
+		await app.Page.Initial_Zoom_Move();
+	}
+	if(App_Config.HAVE_STATION_SCHEDULES)
+	{
+		throw new Error("Not yet implemented");
+	}
+	if(App_Config.HAVE_LINE_TIMETABLE)
+	{
+		let timetable_page = Timetables_Page.Create();
+		let app = App.Create(loader,timetable_page,null);
+		app.Loading();
+		timetable_page.Initialize_Timetable();
+		app.Loaded();
+	}
 }
 
 Initialize();
