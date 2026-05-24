1e3e9dc012f6412ff7e1d55fccc080f59fb1a83b
leo <l.nium@hotmail.fr>
2026-05-24 22:37:47 +0200

    30 - add lint

---
+++ prettier.config.cjs
@@ -0,0 +1,10 @@
+/** @type {import("prettier").Config} */
+module.exports = {
+	useTabs: true,
+	tabWidth: 4,
+	singleQuote: true,
+	trailingComma: 'es5',
+	printWidth: 100,
+	semi: true,
+	plugins: ['prettier-plugin-svelte']
+};
